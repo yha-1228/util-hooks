1b0ab27ce9ab42d201a150b07bb252d29130702a
yuta <gssbtsm999@gmail.com>
2023-02-24 05:06:58 +0900

    add withCompose

---
+++ src/utils/compose.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode, cloneElement } from 'react';
+
+const compose = (children: ReactNode, ...elements: JSX.Element[]) => {
+  return (
+    <>
+      {elements.reduceRight(
+        (prev, current) => cloneElement(current, current.props, prev),
+        children
+      )}
+    </>
+  );
+};
+
+/**
+ * @example
+ * ```tsx
+ * const Providers = withCompose(
+ *   <AuthProvider />,
+ *   <ThemeProvider default="light" />,
+ *   <LangProvider default="ja" />,
+ *   <QueryClientProvider />
+ * );
+ *
+ * Providers.displayName = "Providers"
+ *
+ * return <Providers>...</Providers>
+ * ```
+ */
+export function withCompose(...elements: JSX.Element[]) {
+  const Composed: React.FC<React.PropsWithChildren> = ({ children }) => {
+    return <>{compose(children, ...elements)}</>;
+  };
+
+  return Composed;
+}
