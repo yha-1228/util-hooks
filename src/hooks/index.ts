a8097bd8fe721c3c9a535478f31a254dab02af0a
yuta <gssbtsm999@gmail.com>
2023-02-25 22:01:21 +0900

    add useConcatRef

--- src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './use-array';
-export * from './use-boolean';
-export * from './use-counter';
-export * from './use-fetch';
--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createContextState } from './utils/context';
-
-function useCount() {
-  const [cnt, setCnt] = useState(5);
-
-  const inc = () => setCnt((prev) => prev + 1);
-
-  return { cnt, inc };
-}
-
-const [useCountCtx, CountProvider] = createContextState(useCount, {
-  hookName: 'useCountCtx',
-  providerName: 'CountProvider',
-});
-
-function Inner() {
-  const res = useCountCtx();
-
-  return <button onClick={res.inc}>{res.cnt}</button>;
-}
-
-function App() {
-  return (
-    <CountProvider>
-      <Inner />
-    </CountProvider>
-  );
-}
+import { App } from './app';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
