89d2db445f665dd5d37bbb9835bfde98e5c244b4
yuta <gssbtsm999@gmail.com>
2024-01-08 03:29:02 +0900

    feat: remove unnecessary hooks

--- src/app.tsx
+++ src/app.tsx
@@ -1,46 +1,8 @@
-import React, { ComponentPropsWithRef } from 'react';
-import { useConcatRef } from './hooks/use-concat-ref';
-
-const Button = React.forwardRef<
-  HTMLButtonElement,
-  ComponentPropsWithRef<'button'>
->((props, outerRef) => {
-  const myRef = React.useRef<HTMLButtonElement>();
-  const ref = useConcatRef(myRef, outerRef);
-
-  return (
-    <div style={{ border: '2px solid red' }}>
-      <button onClick={() => myRef.current?.click()}>Button</button>
-
-      <button {...props} ref={ref} />
-    </div>
-  );
-});
-
-Button.displayName = 'Button';
-
 export function App() {
-  const button1Ref = React.useRef<HTMLButtonElement>(null);
-  const button2Ref = React.useRef<HTMLButtonElement>(null);
-
   return (
     <div>
-      <button
-        ref={button1Ref}
-        onClick={() => {
-          button2Ref.current?.click();
-        }}
-      >
-        Button1
-      </button>
-      <Button
-        ref={button2Ref}
-        onClick={() => {
-          console.log(`Button2 clicked at ${new Date()}`);
-        }}
-      >
-        Button2
-      </Button>
+      <h1>Demo</h1>
+      TODO:
     </div>
   );
 }
