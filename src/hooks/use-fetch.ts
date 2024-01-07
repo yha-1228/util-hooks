b35f07eb8325ce1e059c3456c8b5e5f6d8a3ce8d
yuta <gssbtsm999@gmail.com>
2024-01-08 04:30:01 +0900

    refactor(useFetch): flat file

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,11 +1,93 @@
 import React from 'react';
-import {
-  judgeIsError,
-  judgeIsLoading,
-  judgeIsSuccess,
-  getStatus,
-} from './misc';
-import { UseFetchState, UseFetchOptions, UseFetchResult } from './types';
+
+export type UseFetchOptions<Data> = {
+  fetchFn: () => Promise<Data>;
+  depsKey?: React.DependencyList;
+  enabled?: boolean;
+  initialData?: Data;
+  onSuccess?: (data: Data) => void;
+  onError?: (error: Error) => void;
+  onFinally?: () => void;
+};
+
+export type UseFetchState<Data = unknown> = {
+  data: Data | undefined;
+  isFetching: boolean;
+  error: Error | undefined;
+};
+
+export type Status = 'loading' | 'success' | 'error';
+
+type UseFetchBaseResult<Data = unknown> = UseFetchState<Data> & {
+  status: Status;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  refetch: () => void;
+  setDataForce: (data: Data) => void;
+};
+
+interface UseFetchLoadingResult extends UseFetchBaseResult {
+  status: 'loading';
+  data: undefined;
+  error: undefined;
+  isLoading: true;
+  isSuccess: false;
+  isError: false;
+}
+
+interface UseFetchSuccessResult<Data> extends UseFetchBaseResult<Data> {
+  status: 'success';
+  data: Data;
+  isLoading: false;
+  isSuccess: true;
+  isError: false;
+}
+
+interface UseFetchErrorResult<Data> extends UseFetchBaseResult<Data> {
+  status: 'error';
+  error: Error;
+  isLoading: false;
+  isSuccess: false;
+  isError: true;
+}
+
+export type UseFetchResult<Data> =
+  | UseFetchLoadingResult
+  | UseFetchSuccessResult<Data>
+  | UseFetchErrorResult<Data>;
+
+export const judgeIsLoading = <Data>(state: UseFetchState<Data>) => {
+  return !state.data && !state.error;
+};
+
+export const judgeIsSuccess = <Data>(state: UseFetchState<Data>) => {
+  return !!state.data;
+};
+
+export const judgeIsError = <Data>(state: UseFetchState<Data>) => {
+  return !!state.error;
+};
+
+class UnfedinedStatusError extends Error {
+  constructor(...params: Parameters<ErrorConstructor>) {
+    super(...params);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, UnfedinedStatusError);
+    }
+
+    this.name = 'UnfedinedStatusError';
+  }
+}
+
+export const getStatus = <Data>(state: UseFetchState<Data>): Status => {
+  if (judgeIsLoading(state)) return 'loading';
+  if (judgeIsError(state)) return 'error';
+  if (judgeIsSuccess(state)) return 'success';
+
+  throw new UnfedinedStatusError(`Undefined status`);
+};
 
 function useFetch<Data>(options: UseFetchOptions<Data>): UseFetchResult<Data> {
   const {
